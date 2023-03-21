0469e9bac4f83367b2dcb4207a8e5aec100f4913
Jong Hoon Kim <zplanet@hotmail.com>
2023-03-21 18:43:44 -0400

    Add amplify

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import { Amplify } from "aws-amplify";
+import awsConfig from './aws-exports';
 
-function App() {
+Amplify.configure(awsConfig);
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Authenticator>
+      {(props) => {
+        const { signOut, user } = props;
+        return (
+          <main>
+            <h1>Hello {user?.attributes?.email}</h1>
+            <button onClick={signOut}>Sign out</button>
+          </main>
+        );
+      }}
+    </Authenticator>
   );
-}
+};
 
 export default App;
