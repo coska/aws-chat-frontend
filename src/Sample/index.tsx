2b48056f1e16333f6da17e3584af1ed03149174d
Junyoung Choi <ccjy1113@gmail.com>
2023-05-10 11:34:14 +0900

    add examples how to use api with cognito

--- src/Sample/index.tsx
+++ src/Sample/index.tsx
@@ -4,6 +4,9 @@ import { currentMenuIndexState, menus, Result, resultsSelector } from './state';
 import SocketComponent from './websocket';
 import { useEffect, useState } from 'react';
 import { Api } from '../api';
+import { UserDto } from '../coska-client';
+import { Auth } from 'aws-amplify';
+
 
 const ResultList = () => {
     const loadable = useRecoilValueLoadable(resultsSelector);
@@ -28,22 +31,78 @@ const ResultList = () => {
     }
 }
 
+function generateGuid() {
+    let guid = '';
+    const hexChars = '0123456789abcdef';
+
+    for (let i = 0; i < 36; i++) {
+        if (i === 8 || i === 13 || i === 18 || i === 23) {
+            guid += '-';
+        } else if (i === 14) {
+            guid += '4';
+        } else {
+            const randomHex = Math.floor(Math.random() * 16);
+            guid += hexChars[randomHex];
+        }
+    }
+
+    return guid;
+}
+
+
 function MyComponent() {
     const [email, setEmail] = useState('');
+    const [userList, setUserList] = useState('');
+    const [token, setToken] = useState('');
+    const [insertedUser, setInsertUser] = useState("");
     useEffect(() => {
-        console.log(`${email}`);
+        // console.log(`${email}`);
     }, [email]);
+
+    const getToken = async () => {
+        const session = await Auth.currentSession();
+
+        setToken(session.getIdToken().getJwtToken());
+    }
 
     const getMyEmail = async () => {
         const resp = await Api.SecurityTest?.getSecurityTest();
-        setEmail(resp?.data ?? "");
-        console.log(resp?.data);
+        setEmail(resp?.data);
+
+        console.log(generateGuid());
     };
+
+    const getUserList = async () => {
+        const userList = await Api.User.findAll();
+
+        setUserList(JSON.stringify(userList?.data));
+    }
+
+    const insertUser = async () => {
+        const userDto: UserDto = {
+            id: generateGuid(),
+            firstName: Math.random().toString(36).substring(2, 15),
+            lastName: Math.random().toString(36).substring(2, 15)
+        }
+        const resp = await Api.User.create(userDto);
+
+        console.log(resp.data);
+        setInsertUser(JSON.stringify(resp.data));
+
+    }
 
     return (
         <div>
+            <p>ENV_TEST_VARIABLE: {process.env.REACT_APP_CHAT_REMOTE_URL}</p>
+
             <p>email: {email}</p>
             <button onClick={getMyEmail}>getMyEmail</button>
+            <p>token: {token}</p>
+            <button onClick={getToken}>getToken</button>
+            <p>userList: {userList}</p>
+            <button onClick={getUserList}>getUserList</button>
+            <p>insert user: {insertedUser}</p>
+            <button onClick={insertUser}>insertUser</button>
         </div>
     );
 
